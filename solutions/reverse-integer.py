dd70458148b30352910599c72c71d585b95256df
Liang Shuailong <1000829@Liangs-MacBook-Pro.local>
2016-01-15 16:48:00 +0800

    finish reverse_integer

--- solutions/reverse-integer.py
+++ solutions/reverse-integer.py
@@ -10,16 +10,62 @@ https://leetcode.com/problems/reverse-integer/.
 
 """
 
+'''
+Note1: Python sys.maxint = 2**64-1
+Note2: Solution and Solution1 are both accepted. Solution is quicker.
+
+'''
+
 class Solution(object):
     def reverse(self, x):
         """
         :type x: int
         :rtype: int
         """
+        neg = False
+        if x < 0:
+        	x = -x
+        	neg = True
+
+        s = str(x)[::-1]
+
+        if neg:
+        	s = '-' + s
+
+        s = int(s)
+        MAX = 2**31-1
+        if s > MAX or s < -MAX:
+        	return 0
+        
+        return s
+
+class Solution1(object):
+    def reverse(self, x):
+        """
+        :type x: int
+        :rtype: int
+        """
+        MAX = 2**31-1
+        res = 0
+        neg = False
+        if x < 0:
+        	x  = -x
+        	neg = True
+        while x != 0:
+        	res = res * 10 + x % 10
+        	x /= 10
+        if neg:
+        	res = -res
+        if res > MAX or res < -MAX:
+        	return 0
+        
+        return res
+
         
 def main():
     solution = Solution()
-    print solution.reverse()
+ 
+    print solution.reverse(-123456789)
     
 if __name__ == '__main__':
     main()
