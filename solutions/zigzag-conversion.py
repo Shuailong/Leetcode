625a9a63845cb043e9be071ee4760a40c39c87b8
Liang Shuailong <liangshuailong@gmail.com>
2016-03-21 22:06:24 +0800

    solve 1 problem

--- solutions/zigzag-conversion.py
+++ solutions/zigzag-conversion.py
@@ -10,11 +10,36 @@ https://leetcode.com/problems/zigzag-conversion/.
 
 """
 
+'''
+Key: understand the problem.
+'''
+
+class Solution(object):
+    def convert(self, s, numRows):
+        """
+        :type s: str
+        :type numRows: int
+        :rtype: str
+        """
+        if numRows == 1:
+            return s
+        res = [''] * numRows
+        period = numRows + (numRows - 2)
+        for i in range(len(s)):
+            floor = i % period
+            if floor > period/2:
+                floor = period - floor
+            res[floor] += s[i]
+        return ''.join(res)
+
+
 
 def main():
-    pass
+    solution = Solution()
+    s = 'PAYPALISHIRING'
+    numRows = 3 
+    print solution.convert(s, numRows) == 'PAHNAPLSIIGYIR'
     
 if __name__ == '__main__':
     main()
 
-        
